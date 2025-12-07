4df3ded87b89fe3b80e6b7bdbdc5ba24e312ab07
Thiago Dias <thiago.dsd@outlook.com>
2025-12-07 20:12:31 -0300

    add Node class with constructor for nome and idade attributes

---
+++ node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, nome, idade):
+        self.nome = nome          
+        self.idade = idade        
+        self.esquerda = None     
+        self.direita = None      
